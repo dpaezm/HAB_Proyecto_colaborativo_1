f166db8753f6168130dbd6e11932f31efa643476
Marc Ganella <marcgx8@gmail.com>
2025-01-20 21:50:28 +0100

    Funcion crearRespuestas a través de array

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+function crearRespuestas(array, correcta) {
+  const listaBotones = document.getElementById("answers");
+
+  const arrayRespuestas = [...array];
+
+  arrayRespuestas.forEach((texto, index) => {
+    //Crear elemento li
+    const li = document.createElement("li");
+
+    //Crear botón
+
+    const boton = document.createElement("button");
+    boton.textContent = texto;
+
+    //Funcion que comparara respuestas correctas e incorrectas
+    boton.addEventListener("click", () => {
+      if (texto === correcta) {
+        console.log("Respuesta Correcta!");
+      } else {
+        console.log("Respuesta Incorrecta!");
+      }
+    });
+
+    //Añadimos boton al li yy despues el bloque completo a answers
+    li.appendChild(boton);
+    listaBotones.appendChild(li);
+  });
+}
+
+crearRespuestas(
+  ["Respuesta 1", "Respuesta 2", "Respuesta 3", "Respuesta 4"],
+  "Respuesta 2"
+);
+
+// function isRespuestaCorrecta(texto, correcta) {
+
+// }
